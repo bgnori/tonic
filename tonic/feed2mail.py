e7c4b61dca205a9bffbfa57a4592770fbf3002cf
Noriyuki Hosaka <bgnori@gmail.com>
2009-03-07 17:50:19 +0900

    supported differential posting of message. now it posts new items only.

--- tonic/feed2mail.py
+++ tonic/feed2mail.py
@@ -5,9 +5,12 @@
 # Copyright 2009 Noriyuki Hosaka bgnori@gmail.com
 #
 
+import os.path
 import sys
 import time
 from datetime import datetime as dt
+import pickle
+
 import urllib
 import feedparser
 import smtplib
@@ -16,35 +19,53 @@ from email.Utils import formatdate
 
 
 class Bot(object):
-  def __init__(self, feed_url, bot_addr, 
-                     sender_addr, password, server, grp_addr,
-                     out=None):
+  def __init__(self, out=None, **kw):
+  #feed_url, bot_addr, sender_addr, password, server, grp_addr,
     if out is None:
       out = sys.stdout
-    self.feed_url = feed_url
-    self.bot_addr = bot_addr
-    self.sender_addr = sender_addr
-    self.password = password
-    self.grp_addr = grp_addr
-    self.server = server
     self.out = out
+    self.depot = kw
+
+  def __getattr__(self, name):
+    assert name in ("feed_url", "bot_addr", 
+                    "sender_addr", "password", 
+                    "grp_addr", "server",
+                    "mailbody", "mailsubject",
+                    "last",)
+    return self.__dict__['depot'][name]
 
   def write(self, s):
     self.out.write(s)
 
   def run(self):
     feed = self.get(self.feed_url)
-    tobepost = self.check(feed)
-    if not tobepost:
-      self.write('no new post found.\n')
-      return
-    self.write('%i new posts found.\n'%(len(tobepost)))
 
-    messages = self.make_messages(tobepost)
 
     self.write('connecting to mail server.\n')
-    self.mail(messages)
-    self.write('sent.\n')
+
+    con = smtplib.SMTP(self.server)
+    c = 0
+    try:
+      con.ehlo()
+      con.starttls()
+      con.ehlo()
+      con.login(self.sender_addr, self.password)
+      self.write('sending with %s to %s\n'
+                  %(self.sender_addr, self.grp_addr))
+      for entry in feed.entries:
+        generated = dt(*entry.updated_parsed[:5])
+        if generated > self.lastupdate():
+          msg = self.make_message(entry)
+          con.sendmail(self.sender_addr, self.grp_addr, msg.as_string())
+          self.update(generated)
+          c += 1
+          self.write('.')
+    finally:
+      con.close()
+    if c > 0:
+      self.write('sent.\n')
+    else:
+      self.write('no item to work with.\n')
     self.write('done.\n')
     return 
 
@@ -56,39 +77,34 @@ class Bot(object):
     finally:
       f.close()
 
-  def check(self, feed):
-    tobepost = []
-    now = dt.utcnow()
-    for entry in feed.entries:
-      generated = dt(*entry.updated_parsed[:5])
-      delta = generated - now
-      #if delta.days == 0 and delta.seconds < 3600:
-      tobepost.append(entry)
-    return tobepost
+  def lastupdate(self):
+    path = os.path.abspath(self.last)
+    if not os.path.exists(path):
+      return dt(1900, 1, 1)
+    else:
+      f = file(path)
+      try:
+        t = pickle.load(f)
+      finally:
+        f.close()
+      assert isinstance(t, dt)
+      return t
 
-  def make_messages(self, tobepost):
-    messages = []
-    for entry in tobepost:
-      msg = MIMEText(entry.summary.encode('utf-8'), 'plain', 'utf-8')
-      msg['Subject'] = entry.title.encode('utf-8')
-      msg['From'] = self.bot_addr 
-      msg['To'] = self.grp_addr 
-      msg['Date'] = formatdate()
-      messages.append(msg)
-    return messages
-
-  def mail(self, messages):
-    con = smtplib.SMTP(self.server)
+  def update(self, t):
+    assert isinstance(t, dt)
+    path = os.path.abspath(self.last)
+    f = file(path, 'w')
     try:
-      con.ehlo()
-      con.starttls()
-      con.ehlo()
-      con.login(self.sender_addr, self.password)
-      self.write('sending with %s to %s\n'
-                  %(self.sender_addr, self.grp_addr))
-      for msg in messages:
-        con.sendmail(self.sender_addr, self.grp_addr, msg.as_string())
-        self.write('.')
+      pickle.dump(t, f)
     finally:
-      con.close()
+      f.close()
+    
+  def make_message(self, entry):
+    msg = MIMEText(self.mailbody(entry).encode('utf-8'), 'plain', 'utf-8')
+    msg['Subject'] = self.mailsubject(entry).encode('utf-8')
+    msg['From'] = self.bot_addr 
+    msg['To'] = self.grp_addr 
+    msg['Date'] = formatdate()
+    return msg
+
 
--- tonic/feed2mail_test.py
+++ tonic/feed2mail_test.py
@@ -5,35 +5,54 @@
 # Copyright 2008 Noriyuki Hosaka bgnori@gmail.com
 #
 
+import StringIO
 import unittest
+import os
 from tonic.feed2mail import Bot
 
 class BotTest(unittest.TestCase):
   def setUp(self):
     self.bot = Bot(
-          'feed_url',
-          'bot_addr',
-          'sender_addr', 
-          'password', 
-          'grp_addr',
-          'server',
+          feed_url='feed_url',
+          bot_addr='bot_addr',
+          sender_addr='sender_addr', 
+          password='password', 
+          grp_addr='grp_addr',
+          server='server',
+          storage=StringIO.StringIO(),
+          mailbody = lambda x: x.content[0].value,
+          mailsubject = lambda x: x.title,
+          last='feed2mail_lastfile',
           )
 
   def tearDown(self):
-    pass
+    try:
+      os.remove('feed2mail_lastfile')
+    except:
+      pass
 
   def test_get(self):
-    self.bot.get('http://nori-on-baking.blogspot.com/feeds/posts/default')
+    print self.bot.get('http://nori-on-baking.blogspot.com/feeds/posts/default')
 
   def test_check(self):
     feed = self.bot.get('http://nori-on-baking.blogspot.com/feeds/posts/default')
     new = self.bot.check(feed)
+    print new
     self.assert_(len(new))
 
   def test_make_message(self):
     feed = self.bot.get('http://nori-on-baking.blogspot.com/feeds/posts/default')
-    new = self.bot.check(feed)
-    msgs = self.bot.make_messages(new)
+    news = self.bot.check(feed)
+    msgs = self.bot.make_messages(news)
+    for m in msgs:
+      print '='*60
+      print m
+      print '='*60
+      self.assertEqual(m['From'], 'bot_addr')
+      self.assertEqual(m['To'], 'grp_addr')
+      self.assertEqual(m['Content-Type'], 'text/plain; charset="utf-8"')
 
-
+  def test_mail(self):
+    pass
+      
 
