4d0af84e787e4f6694e6939f875adbaace544672
Noriyuki Hosaka <bgnori@gmail.com>
2009-03-19 11:27:00 +0900

    achieved 100% coverage for tonic.combination moved test generating source to make_test_combination.py dropped unused. 	modified:   tonic/combination.py 	modified:   tonic/combination_test.py 	new file:   tonic/make_test_combination.py

--- tonic/combination.py
+++ tonic/combination.py
@@ -21,23 +21,21 @@ def fact(n):
     return 1
   elif n == 1:
     return 1
-  elif n > 1:
-    return n*fact(n-1)
   else:
-    assert False
+    assert n > 1
+    return n*fact(n-1)
 
 
 def C(n, r):
-  assert n >0
-  assert r >0
+  assert n > 0
+  assert r > 0
   if n > r:
     return fact(n)/(fact(r) * fact(n-r))
   elif r > n:
     return fact(r)/(fact(n) * fact(r-n))
-  elif n == r:
-    return 1
   else:
-    assert False
+    assert n == r
+    return 1
 
 
 def C_Hash(xs, r):
@@ -59,20 +57,6 @@ def C_Hash(xs, r):
   return hash
 
 
-
-def C_RHash_naive(h, n, r):
-  '''Reverse function of C_Hash.
-    Parameters are hash value h, and n, r of C(n, r)
-    retuns xs
-
-    this provides very naive implementation
-  '''
-  for i in range(2**n):
-    xs = [i & 1 << mask and 1 or 0 for mask in range(n)]
-    if sum(xs) == r:
-      if h == C_Hash(xs, r):
-        return tuple(xs)
-
 def C_RHash(h, n, r):
   '''Reverse function of C_Hash.
     Parameters are hash value h, and n, r of C(n, r)
@@ -88,72 +72,4 @@ def C_RHash(h, n, r):
     return (1, ) + C_RHash(h - C(n-1, r), n-1, r-1)
   else:
     return (0, ) + C_RHash(h, n-1, r)
-
-
-if __name__ == '__main__':
-  ''' unittest generator '''
-  print ('''\
-#!/usr/bin/env python
-# -*- coding: us-ascii -*-
-# vim: syntax=python
-#
-# Copyright 2008 Noriyuki Hosaka bgnori@gmail.com
-#
-# DO NOT EDIT. THIS FILE IS PROGRAM GENERATED.
-#
-
-import unittest
-
-from tonic.combination import *
-'''
-  )
-  def sum(xs):
-    s = 0
-    for n in xs:
-      s +=n
-    return s
-
-  def makeTestCase(n, r):
-    print '''\
-class HashTest_n%i_r%i(unittest.TestCase):'''%(n, r)
-    for i in range(2**n):
-      xs = [i & 1 << mask and 1 or 0 for mask in range(n)] 
-      if sum(xs) == r:
-        print '''\
-  def test_C_Hash_range_%s(self):'''%(''.join(['%i'%x for x in xs]))
-        print '''\
-      h = C_Hash(%s, %i)'''%(xs, r)
-        print '''\
-      self.assert_(C(%i, %i)> h)'''%(n, r)
-        print '''\
-      self.assert_(h>=0)'''
-        print
-
-    print '''\
-  def test_C_Hash_id(self):'''
-    print '''\
-    d = dict()'''
-    for i in range(2**n):
-      xs = [i & 1 << mask and 1 or 0 for mask in range(n)] 
-      if sum(xs) == r:
-        print '''\
-    self.assert_(d.get(C_Hash(%s, %i), None) is None)'''%(xs, r)
-        print '''\
-    d[C_Hash(%s, %i)] = %s'''%(xs, r, xs)
-    print '''\
-    self.assertEqual(len(d), %i)'''%(C(n, r))
-    print
-
-    for i in range(2**n):
-      xs = [i & 1 << mask and 1 or 0 for mask in range(n)] 
-      if sum(xs) == r:
-        print '''\
-  def test_C_RHash_soundness_%s(self):'''%(''.join(['%i'%x for x in xs]))
-        print '''\
-      self.assertEqual(C_RHash(%i, %i, %i), tuple(%s))'''%(C_Hash(xs, r), n, r, xs)
-        print
-
-  for n in range(12):
-    for r in range(1, n):
-      makeTestCase(n, r)
 
